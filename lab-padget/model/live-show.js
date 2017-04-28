6d5e3b127b4b888cdb74aab626693b925fe33987
Michael Padget <mpadget@comcast.net>
2017-04-27 18:06:19 -0700

    Installed npm packages

---
+++ lab-padget/model/live-show.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const debug = require('debug')('http:live-show'); // was >> http:kid-toy
+console.log(debug);
+const uuid = require('uuid/v4');
+
+// http POST :3000/api/music artist=“Lala” album=”GetReal” song=“MakeBelieve”
+module.exports = function(artist, album, song) {
+  if(!artist || !album) throw new Error('Invalid arguments');
+  // Hazard property will be true. In this case of song.
+  this.artist = artist; // was >> name
+  this.album = album; // was >> type
+  this.song = song; // was >> hazard
+  this.id = uuid();
+};
+
+// const debug = require('debug')('http:live-show');
+// const uuid = require('uuid/v4');
+//
+// module.exports = function(name, type, hazard=true) {
+//   if(!name || !type) throw new Error('Invalid arguments');
+//   this.name = name;
+//   this.type = type;
+//   this.hazard = hazard;
+//   this.id = uuid();
+// };
