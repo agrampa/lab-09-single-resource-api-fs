5fa456a36793089ec83fdaf6d3f399c0d7559931
Abigail White <abigailwhite213@gmail.com>
2017-05-02 09:33:06 -0700

    PUT WORKING PRAISE GOD

--- lab-abigail/lib/storage.js
+++ lab-abigail/lib/storage.js
@@ -16,23 +16,41 @@ exports.createItem = function(schema, food) {
 
   storage[schema][food.id] = food;
 
-  fs.writeFileProm(`./data/${food.id}.txt`, JSON.stringify(food))
+  fs.writeFileProm(`${__dirname}/../data/${food.id}.json`, JSON.stringify(food))
   .then( (food) => {
   })
   .catch(console.error);
 };
 
-exports.updateItem = function(schema, id) {
+exports.updateItem = function(schema, id, newFood) {
   debug('#updateItem');
 
-  if(!schema) return reject(new Error('shema required'));
-  if(!id) return reject(new Error('id required'));
+  if(!schema) return Promise.reject(new Error('schema required'));
+  if(!id) return Promise.reject(new Error('id required'));
+  // if(!storage[schema]) storage[schema] = {};
 
-  return fs.readFileProm(`./data/${id}.txt`)
+  // let food = storage[schema][id];
+
+  // let schemaName = storage[schema];
+  // console.log(schemaName);
+  // if (!schemaName) return Promise.reject(new Error('Schema not found'));
+  //
+  // let food = schemaName[id];
+  // if (!food) return Promise.reject(new Error('Food not found'));
+  // console.log(food.id);
+
+
+  return fs.readFileProm(`${__dirname}/../data/${id}.json`)
   .then( food => {
-    fs.writeFileProm(`./data/${id}.txt`, JSON.stringify(food))
-    .then( (food) => {
-    })
+    console.log('I made it!');
+    let stringFood = JSON.parse(food.toString());
+    if (newFood.name) stringFood.name = newFood.name;
+    if (newFood.type) stringFood.type = newFood.type;
+    if (newFood.cost) stringFood.cost = newFood.cost;
+    fs.writeFileProm(`${__dirname}/../data/${id}.json`, JSON.stringify(stringFood))
+    // .then( (food) => {
+      // console.log(food);
+    // })
     .catch(console.error);
   })
   .catch(console.error);
@@ -40,10 +58,10 @@ exports.updateItem = function(schema, id) {
 
 exports.fetchItem = function(schema, id) {
   debug('#fetchItem');
-  if(!schema) return reject(new Error('shema required'));
-  if(!id) return reject(new Error('id required'));
+  if(!schema) return Promise.reject(new Error('shema required'));
+  if(!id) return Promise.reject(new Error('id required'));
 
-  return fs.readFileProm(`./data/${id}.txt`)
+  return fs.readFileProm(`${__dirname}/../data/${id}.json`)
   .then(food => {
     return JSON.parse(food);
   })
@@ -53,10 +71,10 @@ exports.fetchItem = function(schema, id) {
 exports.deleteItem = function(schema, id) {
   debug('#deleteItem');
 
-  if(!schema) return reject(new Error('schema required'));
-  if(!id) return reject(new Error('id required'));
+  if(!schema) return Promise.reject(new Error('schema required'));
+  if(!id) return Promise.reject(new Error('id required'));
 
-  return fs.unlinkProm(`./data/${id}.txt`)
+  return fs.unlinkProm(`${__dirname}/../data/${id}.json`)
   .then(food => {
     console.log('Food deleted');
   })
