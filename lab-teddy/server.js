10b8ae55bc1c676b84b3ac1e9a6a960da47de793
David Teddy <davidteddy@outlook.com>
2017-05-02 11:19:13 -0700

    #

--- lab-teddy/server.js
+++ lab-teddy/server.js
@@ -5,5 +5,5 @@ const Router = require('./lib/router');
 const PORT = process.env.PORT || 8080;
 const router = new Router();
 const server = module.exports = http.createServer(router.route());
-require('./routes/toy-routes')(router);
+require('./routes/car-routes')(router);
 server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
