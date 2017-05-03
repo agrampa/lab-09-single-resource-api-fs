a2ff5e2658adcdb3b7b68acdf0fbc3598063493b
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-05-02 21:27:44 -0700

    added gitignore eslintrc etc

--- lab-lindsay/routes/candy-routes.js
+++ lab-lindsay/routes/candy-routes.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const storage = require(`${__dirname}/../lib/storage`);
-const Candy = require(`${__dirname}/../model/candy`);
-const debug = require('debug')('http:server');
+const storage = require('../lib/storage');
+const Candy = require('../model/candy');
+const debug = require('debug')('http:candy-routes');
 
 module.exports = function(router) {
-
   router.get('/api/candy', function(req, res) {
     debug('GET /api/candy');
     if(req.url.query.id) {
-      storage.fetchItem('candy', req.url.query.id)
+      storage.fetchCandy('candy', req.url.query.id)
       .then(candy => {
-        res.writeHead(201, {'Content-Type': 'application/json'});
+        res.writeHead(200, {'Content-Type': 'application/json'});
         res.write(JSON.stringify(candy));
         res.end();
       })
@@ -30,15 +29,16 @@ module.exports = function(router) {
 
   router.post('/api/candy', function(req, res) {
     debug('POST /api/candy');
-
     try {
-      let candyData = new Candy(req.body.name, req.body.type, req.body.texture);
-      storage.createItem('candy', candyData);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(JSON.stringify(candyData));
-      res.end();
-    } catch(e) {
-      console.error(e);
+      let candy = new Candy(req.body.name, req.body.type, req.body.texture);
+      storage.createCandy('candy', candy)
+      .then(newCandy => {
+        res.writeHead(201, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify(newCandy));
+        res.end();
+      });
+    } catch(err) {
+      console.error(err);
       res.writeHead(400, {'Content-Type' : 'text/plain'});
       res.write('bad request');
       res.end();
@@ -48,15 +48,15 @@ module.exports = function(router) {
   router.delete('/api/candy', function(req, res) {
     debug('DELETE /api/candy');
     if(req.url.query.id) {
-      storage.removeItem('candy', req.url.query.id)
-      .then(candy => {
-        res.writeHead(404, {'Content-Type' : 'application/json'});
+      storage.removeCandy('candy', req.url.query.id)
+      .then(() => {
+        res.writeHead(204, {'Content-Type' : 'application/json'});
         res.write('candy deleted successfully');
         res.end();
       })
       .catch(err => {
         console.error(err);
-        res.writeHead(404, {'Content-Type' : 'application/json'});
+        res.writeHead(404, {'Content-Type' : 'text/plain'});
         res.write('not found');
         res.end();
       });
@@ -69,28 +69,18 @@ module.exports = function(router) {
 
   router.put('/api/candy', function(req, res) {
     debug('PUT /api/candy');
-    let id = req.url.query.id;
-
-    if(id) {
-      storage.updateItem('candy', id)
-      .then(candy => {
-        if(req.body.name) candy.name = req.body.name;
-        if(req.body.type) candy.type = req.body.type;
-        if(req.body.texture) candy.texture = req.body.texture;
-        res.writeHead(201, {'Content-Type' : 'application/json'});
-        res.write(JSON.stringify(candy));
-        res.end();
-      })
-      .catch(err => {
-        console.error(err);
-        res.writeHead(404, {'Content-Type' : 'text/plain'});
-        res.write('update item not found');
-        res.end();
-      });
-      return;
-    }
-    res.writeHead(400, {'Content-Type' : 'text/plain'});
-    res.write('bad request');
-    res.end();
+    storage.updateCandy('candy', req.body.id, req.body)
+    .then(candy => {
+      res.writeHead(202, {'Content-Type' : 'text/plain'});
+      res.write(JSON.stringify(candy));
+      res.end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.writeHead(400, {'Content-Type' : 'text/plain'});
+      res.write('bad request');
+      res.end();
+    });
+    return;
   });
 };
